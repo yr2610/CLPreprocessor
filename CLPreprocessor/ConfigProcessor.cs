8aec350d5422709ccf0f057a2aa55cd740969ec5
yr2610 <shin.nakaji@nifty.com>
2025-01-23 23:10:09 +0900

    yaml に文字列で書いたfunctionを実行できるようにいろいろ修正中

--- CLPreprocessor/ConfigProcessor.cs
+++ CLPreprocessor/ConfigProcessor.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.ClearScript;
 using Microsoft.ClearScript.V8;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NodeTypeResolvers;
+using YamlDotNet.RepresentationModel;
 
 public class ConfigProcessor
 {
     private static readonly Regex TemplateRegex = new Regex(@"\{\{=([\s\S]+?)\}\}", RegexOptions.Compiled);
-    private readonly Dictionary<string, object> _variables = new Dictionary<string, object>();
     private readonly List<ScriptObject> _postProcessFunctions = new List<ScriptObject>();
+    private readonly V8ScriptEngine _engine;
 
-    public Dictionary<string, object> ReadConfigFile(string confFileName)
+    public ConfigProcessor()
+    {
+        _engine = new V8ScriptEngine();
+        LoadLodash();
+    }
+
+    private void LoadLodash()
+    {
+        var exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var lodashPath = Path.Combine(exeDirectory, "scripts", "lodash.min.js");
+        var lodashScript = File.ReadAllText(lodashPath);
+        _engine.Execute(lodashScript);
+    }
+
+    public dynamic ReadConfigFile(string confFileName)
     {
         var data = ReadYamlFile(confFileName);
 
@@ -25,94 +44,100 @@ public class ConfigProcessor
         return data;
     }
 
-    private Dictionary<string, object> ReadYamlFile(string filePath)
+    private dynamic ReadYamlFile(string filePath)
     {
         if (!File.Exists(filePath))
         {
-            return new Dictionary<string, object>();
+            return new ExpandoObject();
         }
 
         var deserializer = new DeserializerBuilder()
+            .WithNodeTypeResolver(new ExpandoObjectNodeTypeResolver())
             .Build();
 
         using (var reader = new StreamReader(filePath))
         {
-            return deserializer.Deserialize<Dictionary<string, object>>(reader);
+            var yaml = reader.ReadToEnd();
+            var yamlStream = new YamlStream();
+            yamlStream.Load(new StringReader(yaml));
+
+            if (yamlStream.Documents.Count == 0)
+            {
+                return new ExpandoObject();
+            }
+
+            var rootNode = yamlStream.Documents[0].RootNode;
+            if (rootNode is YamlSequenceNode)
+            {
+                return deserializer.Deserialize<List<ExpandoObject>>(new StringReader(yaml));
+            }
+            else if (rootNode is YamlMappingNode)
+            {
+                return deserializer.Deserialize<ExpandoObject>(new StringReader(yaml));
+            }
+            else
+            {
+                throw new YamlException("Unexpected YAML structure.");
+            }
         }
     }
 
-    private void ProcessIncludeFiles(Dictionary<string, object> data, string baseDirectory)
+    private void ProcessIncludeFiles(dynamic data, string baseDirectory)
     {
-        if (data.ContainsKey("$include"))
+        var dataDict = (IDictionary<string, object>)data;
+
+        if (dataDict.ContainsKey("$include"))
         {
-            var includeFiles = (List<object>)data["$include"];
-            data.Remove("$include");
+            var includeFiles = (List<object>)dataDict["$include"];
+            dataDict.Remove("$include");
 
             foreach (var includeFile in includeFiles)
             {
                 var includeFilePath = Path.Combine(baseDirectory, includeFile.ToString());
                 var includeData = ReadYamlFile(includeFilePath);
                 ProcessIncludeFiles(includeData, Path.GetDirectoryName(includeFilePath));
-                foreach (var kvp in includeData)
+                foreach (var kvp in (IDictionary<string, object>)includeData)
                 {
-                    if (!data.ContainsKey(kvp.Key))
+                    if (!dataDict.ContainsKey(kvp.Key))
                     {
-                        data[kvp.Key] = kvp.Value;
+                        dataDict[kvp.Key] = kvp.Value;
                     }
                 }
             }
         }
 
-        if (data.ContainsKey("$post_process"))
+        if (dataDict.ContainsKey("$post_process"))
         {
-            var script = data["$post_process"].ToString();
+            var script = dataDict["$post_process"].ToString();
             AddPostProcessFunction(script);
-            data.Remove("$post_process");
+            dataDict.Remove("$post_process");
         }
     }
 
     private void AddPostProcessFunction(string script)
     {
-        using (var engine = new V8ScriptEngine())
+        // 関数を文字列として渡し、関数オブジェクトとして保持
+        var functionScript = $"var postProcessFunction = {script}; postProcessFunction;";
+        var functionObject = _engine.Evaluate(functionScript) as ScriptObject;
+        if (functionObject != null)
         {
-            // Lodashのスクリプトを読み込んで実行
-            var exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            var lodashPath = Path.Combine(exeDirectory, "scripts", "lodash.min.js");
-            var lodashScript = File.ReadAllText(lodashPath);
-            engine.Execute(lodashScript);
-
-            // 関数を文字列として渡し、関数オブジェクトとして保持
-            var functionScript = $"var postProcessFunction = {script}; postProcessFunction;";
-            var functionObject = engine.Evaluate(functionScript) as ScriptObject;
-            if (functionObject != null)
-            {
-                _postProcessFunctions.Add(functionObject);
-            }
+            _postProcessFunctions.Add(functionObject);
         }
     }
 
-    private void ExecutePostProcessFunctions(Dictionary<string, object> data)
+    private void ExecutePostProcessFunctions(dynamic data)
     {
-        using (var engine = new V8ScriptEngine())
+        _engine.AddHostObject("data", data);
+
+        // 保持しているpostProcess関数を実行
+        foreach (var function in _postProcessFunctions)
         {
-            engine.AddHostObject("data", data);
-
-            // Lodashのスクリプトを読み込んで実行
-            var exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            var lodashPath = Path.Combine(exeDirectory, "scripts", "lodash.min.js");
-            var lodashScript = File.ReadAllText(lodashPath);
-            engine.Execute(lodashScript);
-
-            // 保持しているpostProcess関数を実行
-            foreach (var function in _postProcessFunctions)
-            {
-                engine.Script.postProcessFunction = function;
-                engine.Execute("postProcessFunction(data);");
-            }
+            _engine.Script.postProcessFunction = function;
+            _engine.Execute("postProcessFunction(data);");
         }
     }
 
-    private void ProcessTemplates(Dictionary<string, object> rootData, Dictionary<string, object> data)
+    private void ProcessTemplates(dynamic rootData, dynamic data)
     {
         var finished = new HashSet<string>();
         bool modified;
@@ -120,14 +145,14 @@ public class ConfigProcessor
         do
         {
             modified = false;
-            foreach (var key in data.Keys.ToList())
+            foreach (var key in ((IDictionary<string, object>)data).Keys.ToList())
             {
                 if (finished.Contains(key))
                 {
                     continue;
                 }
 
-                var value = data[key].ToString();
+                var value = ((IDictionary<string, object>)data)[key].ToString();
                 if (!TemplateRegex.IsMatch(value))
                 {
                     finished.Add(key);
@@ -135,7 +160,7 @@ public class ConfigProcessor
                 }
 
                 var compiled = CompileTemplate(value, rootData);
-                data[key] = compiled;
+                ((IDictionary<string, object>)data)[key] = compiled;
                 modified = true;
             }
         } while (modified);
@@ -143,37 +168,50 @@ public class ConfigProcessor
         CompileForAllChildren(rootData, data);
     }
 
-    private string CompileTemplate(string template, Dictionary<string, object> data)
+    private string CompileTemplate(string template, dynamic data)
     {
         return TemplateRegex.Replace(template, match =>
         {
             var key = match.Groups[1].Value.Trim();
-            return data.ContainsKey(key) ? data[key].ToString() : string.Empty;
+            return ((IDictionary<string, object>)data).ContainsKey(key) ? ((IDictionary<string, object>)data)[key].ToString() : string.Empty;
         });
     }
 
-    private void CompileForAllChildren(Dictionary<string, object> rootData, Dictionary<string, object> data)
+    private void CompileForAllChildren(dynamic rootData, dynamic data)
     {
-        foreach (var key in data.Keys.ToList())
+        foreach (var key in ((IDictionary<string, object>)data).Keys.ToList())
         {
-            if (data[key] is Dictionary<string, object> childData)
+            if (((IDictionary<string, object>)data)[key] is ExpandoObject childData)
             {
                 CompileForAllChildren(rootData, childData);
             }
-            else if (data[key] is List<object> listData)
+            else if (((IDictionary<string, object>)data)[key] is List<object> listData)
             {
                 foreach (var item in listData)
                 {
-                    if (item is Dictionary<string, object> listItemData)
+                    if (item is ExpandoObject listItemData)
                     {
                         CompileForAllChildren(rootData, listItemData);
                     }
                 }
             }
-            else if (data[key] is string strValue && TemplateRegex.IsMatch(strValue))
+            else if (((IDictionary<string, object>)data)[key] is string strValue && TemplateRegex.IsMatch(strValue))
             {
-                data[key] = CompileTemplate(strValue, rootData);
+                ((IDictionary<string, object>)data)[key] = CompileTemplate(strValue, rootData);
             }
         }
+    }
+}
+
+public class ExpandoObjectNodeTypeResolver : INodeTypeResolver
+{
+    public bool Resolve(NodeEvent nodeEvent, ref Type currentType)
+    {
+        if (currentType == typeof(object))
+        {
+            currentType = typeof(ExpandoObject);
+            return true;
+        }
+        return false;
     }
 }
